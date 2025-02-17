fc39dcdc5b720d90b8dcc7dbcec64509f39855b0
andresantossoares <andre.soares7ano@gmail.com>
2025-02-17 15:39:35 -0300

    sua alteração

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int numero;
+     for(numero = 10; numero < 51; numero++){
+         if (numero % 2 == 0) {
+             System.out.println("O numero " + numero +" é par");
+
+         }else{
+             System.out.println("O numero " + numero +" É impar");
+         }
+
+     }
+
+
+    }
+    }
